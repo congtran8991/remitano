7031c3d94584adc7f7cac5972a582676282b2919
congtran8991 <cong.tran@deliveree.com>
2023-04-19 17:20:56 +0700

    change domain name on production

--- client/src/Constants/config.js
+++ client/src/Constants/config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
-const host = dev ? "http://localhost:4000" : "http://localhost:4000";
+const host = dev ? "http://localhost:4000" : "https://remitano-eta.vercel.app/";
 const config = {
 	api_domain: `${host}/api`,
 	file_domain: host,
