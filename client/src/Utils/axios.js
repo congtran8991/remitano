551adb1a9018e90789d09f198b1fb6e2370aae32
congtran8991 <cong.tran@deliveree.com>
2023-04-19 15:24:26 +0700

    Add Axios

---
+++ client/src/Utils/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import appConfig from "../Constants/config";
+export const AXIOS = function (param) {
+  const { path = "/", method = "GET", data = {}, overrideDomain, token = "" } = param;
+  let url = (overrideDomain || appConfig.api_domain) + path;
+  let config = {
+    headers: {
+      "content-type": "application/json",
+      "authorization": token
+    },
+    method,
+    url,
+    data,
+    timeout: 2 * 60 * 1000,
+  };
+  console.log(config)
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then((resp) => {
+        const { data } = resp;
+        resolve(data);
+      })
+      .catch((err) => {
+        console.log("[err when call call api]", err);
+        reject(err.response ? err.response.data : err);
+      });
+  });
+};
