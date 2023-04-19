39b639ba1851b7187c9f2034e0a4d45465a0558b
congtran8991 <cong.tran@deliveree.com>
2023-04-19 23:05:18 +0700

    optimize code api server

--- server/Api/movie.js
+++ server/Api/movie.js
@@ -3,61 +3,66 @@ const movie = require("../Model/movie");
 const user = require("../Model/user");
 const router = express.Router();
 
-router.post("/addListMovie", (req, res) => {
-  const { url, userId } = req.body;
+// router.post("/addListMovie", (req, res) => {
+//   const { url, userId } = req.body;
+//   let dataMovie = new movie({
+//     url: url,
+//     userId,
+//   });
+//   dataMovie
+//     .save()
+//     .then(() => {
+//       return res.status(200).json({
+//         success: true,
+//         message: "Thêm Url thành công",
+//       });
+//     })
+//     .catch((err) => {
+//       res.send({
+//         success: false,
+//         message: "Thêm URL không thành công",
+//         err,
+//       });
+//     });
+// });
+
+router.post("/addListMovie", async (req, res) => {
+  const { url, user } = req.body;
   let dataMovie = new movie({
     url: url,
-    userId,
+    user,
   });
-  dataMovie
-    .save()
-    .then(() => {
-      return res.status(200).json({
-        success: true,
-        message: "Thêm Url thành công",
-      });
-    })
-    .catch((err) => {
-      res.send({
-        success: false,
-        message: "Thêm URL không thành công",
-        err,
-      });
+  try {
+    await dataMovie.save();
+    return res.status(200).json({
+      success: true,
+      message: "Thêm Url thành công",
     });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: "Thêm URL không thành công",
+      err,
+    });
+  }
 });
 
-router.get("/listMovie", (req, res) => {
-  movie
-    .find({})
-    .then((dataMovie) => {
-      user
-        .find({ id: dataMovie.id })
-        .select("email")
-        .then(() => {
-          res.status(200).json({
-            success: true,
-            message: "Lấy danh sách thành công",
-            data: dataMovie,
-          });
-        })
-        .catch((err) => {
-          res.send({
-            success: false,
-            message: "Lấy danh sách không thành công",
-            data: [],
-            err,
-          });
-        });
-      // console.log({ data });
-    })
-    .catch((err) => {
-      res.send({
-        success: false,
-        message: "Lấy danh sách không thành công",
-        data: [],
-        err,
-      });
+router.get("/listMovie", async (req, res) => {
+  try {
+    const dataMovie = await movie.find({});
+    res.status(200).json({
+      success: true,
+      message: "Lấy danh sách thành công",
+      data: dataMovie,
     });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: "Lấy danh sách không thành công",
+      data: [],
+      err,
+    });
+  }
 });
 
 module.exports = router;
--- server/Api/userLogin.js
+++ server/Api/userLogin.js
@@ -1,97 +1,68 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const router = express.Router();
-const login = require("../Model/user");
-const jwt = require("jsonwebtoken");
-const checkToken = require("./checkToken");
-const auth = checkToken.checkToken;
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post("/regis", (req, res) => {
-  let { email, passWord } = req.body;
-  login
-    .findOne({ email })
-    .then((result) => {
-      const payload = {
-        email,
-        id: result.id,
-      };
+const login = require('../Model/user');
+const checkToken = require('./checkToken');
+const auth = checkToken.checkToken;
+const { JWT_SECRET } = process.env;
+
+const router = express.Router();
+
+router.post('/regis', async (req, res) => {
+  const { email, passWord } = req.body;
+
+  try {
+    const user = await login.findOne({ email });
+
+    if (user) {
+      const payload = { email, id: user.id };
       const token = jwt.sign(payload, "SECRET");
-      if (result) {
-        bcrypt
-          .compare(passWord, result?.passWord)
-          .then((resultBcrypt) => {
-            if (resultBcrypt) {
-              return res.status(200).json({
-                success: true,
-                token,
-                message: "Login thành công",
-                data: {
-                  email: result.email,
-                  id: result.id,
-                },
-              });
-            }
-            return res.status(401).json({
-              success: false,
-              message:
-                "Login không thành công. Email hoặc mật khẩu chưa chính sác",
-            });
-          })
-          .catch((err) => {
-            res.send({
-              success: false,
-              message: "Đăng nhập không thành công",
-              err,
-            });
-          });
+
+      const isPasswordMatch = await bcrypt.compare(passWord, user.passWord);
+
+      if (isPasswordMatch) {
+        return res.status(200).json({
+          success: true,
+          token,
+          message: 'Login thành công',
+          data: { email: user.email, id: user.id },
+        });
       } else {
-        bcrypt.genSalt(10, function (err, salt) {
-          bcrypt.hash(passWord, salt, function (err, hash) {
-            let dataRegis = new login({
-              email,
-              passWord: hash,
-            });
-            payload = {
-              email,
-              id: result.id
-            }
-            token = jwt.sign(payload, "SECRET")
-            dataRegis
-              .save()
-              .then((result) => {
-                return res.status(200).json({
-                  success: true,
-                  token,
-                  message: "Đăng kí thành công",
-                  data: {
-                    email,
-                    id: result.id,
-                  },
-                });
-              })
-              .catch((err) =>
-                res.send({
-                  message: "Đăng kí không thành công",
-                  success: false,
-                  err,
-                })
-              );
-          });
+        return res.status(401).json({
+          success: false,
+          message: 'Login không thành công. Email hoặc mật khẩu chưa chính sác',
         });
       }
-    })
-    .catch((err) => {
-      res.send({ success: false, err });
-    });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(passWord, salt);
+
+      const newUser = new login({ email, passWord: hashedPassword });
+      const savedUser = await newUser.save();
+
+      const payload = { email, id: savedUser.id };
+      const token = jwt.sign(payload, "SECRET");
+
+      return res.status(200).json({
+        success: true,
+        token,
+        message: 'Đăng kí thành công',
+        data: { email, id: savedUser.id },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ success: false, message: 'Lỗi server' });
+  }
 });
-router.post("/checkToken", auth, (req, res) => {
+
+router.post('/checkToken', auth, (req, res) => {
   return res.status(200).json({
     success: true,
-    message: "Login thành công",
-    data: {
-      email: req.user.email,
-      id: req.user.id
-    },
+    message: 'Login thành công',
+    data: { email: req.user.email, id: req.user.id },
   });
 });
+
 module.exports = router;
